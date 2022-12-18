d4d11dd4f94c1e446afd934b08bfe08894983c11
hannahk <hanhannah839@gmail.com>
2022-12-17 17:21:54 -0800

    updated changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('../Develop/routes/apiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 // Setting up servers
 const PORT = process.env.PORT || 3001;
